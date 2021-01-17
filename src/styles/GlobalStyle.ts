911eb3346d74e7f3c71875753d9fce55e5824590
hyperflow <hyperflow@kakao.com>
2021-01-18 00:39:09 +0900

    feat(Styles): 글로벌 스타일 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import Color from 'utils/ColorPalette';
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -9,15 +10,34 @@ const GlobalStyle = createGlobalStyle`
   body {
     min-width: 1024px;
     min-height: 100%;
+    background-color: ${Color['gray-0']};
 
     margin: 0;
     padding: 0;
-    font-family: "Inter", Noto Sans KR", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: "Inter", "Noto Sans KR", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  button, button:active {
+    outline: none;
+    border: none;
+
+    font-family: "Inter", "Noto Sans KR", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+  }
+
+  a, a:visited {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
   }
 `;
 
