2bd48e05392311e0a76a5566784e1d00252f19f4
hyperflow <hyperflow@kakao.com>
2021-01-18 22:58:18 +0900

    feat(Api): BoardApi 추가

---
+++ src/apis/BoardApi.ts
@@ -0,0 +1,85 @@
+import { AxiosPromise } from 'axios';
+import Api from './Api';
+import ApiBuilder from './ApiBuilder';
+import config from './config';
+
+const BOARD_API_PREFIX = `${config.API_HOST}/board`;
+
+class BoardApi extends Api {
+  endPoints = {
+    FETCH_BOARD_ALL: `${BOARD_API_PREFIX}`,
+    FETCH_BOARD_DETAIL: (id: number): string => `${BOARD_API_PREFIX}/${id}`,
+    WRITE_BOARD_POST: `${BOARD_API_PREFIX}`,
+    EDIT_BOARD_POST: (id: number): string => `${BOARD_API_PREFIX}/${id}`,
+    WRITE_BOARD_COMMENT: (id: number): string =>
+      `${BOARD_API_PREFIX}/${id}/comment`,
+    EDIT_BOARD_COMMENT: (id: number, commentId: number): string =>
+      `${BOARD_API_PREFIX}/${id}/comment/${commentId}`,
+  };
+
+  fetchBoardAll = (): AxiosPromise => {
+    return ApiBuilder.create()
+      .get()
+      .url(this.endPoints.FETCH_BOARD_ALL)
+      .build();
+  };
+
+  fetchPostDetail = (id: number): AxiosPromise => {
+    return ApiBuilder.create()
+      .get()
+      .url(this.endPoints.FETCH_BOARD_DETAIL(id))
+      .build();
+  };
+
+  writePost = (id: number, postData: any): AxiosPromise => {
+    return ApiBuilder.create()
+      .post()
+      .url(this.endPoints.WRITE_BOARD_POST(id))
+      .data(postData)
+      .build();
+  };
+
+  editPost = (id: number, postData: any): AxiosPromise => {
+    return ApiBuilder.create()
+      .post()
+      .url(this.endPoints.EDIT_BOARD_POST(id))
+      .data(postData)
+      .build();
+  };
+
+  deletePost = (id: number): AxiosPromise => {
+    return ApiBuilder.create()
+      .delete()
+      .url(this.endPoints.EDIT_BOARD_POST(id))
+      .build();
+  };
+
+  writePostComment = (id: number, commentData: any): AxiosPromise => {
+    return ApiBuilder.create()
+      .post()
+      .url(this.endPoints.WRITE_BOARD_COMMENT(id))
+      .data(commentData)
+      .build();
+  };
+
+  editPostComment = (
+    id: number,
+    commentId: number,
+    commentData: any
+  ): AxiosPromise => {
+    return ApiBuilder.create()
+      .post()
+      .url(this.endPoints.EDIT_BOARD_COMMENT(id, commentId))
+      .data(commentData)
+      .build();
+  };
+
+  deletePostComment = (id: number, commentId: number): AxiosPromise => {
+    return ApiBuilder.create()
+      .delete()
+      .url(this.endPoints.EDIT_BOARD_COMMENT(id, commentId))
+      .build();
+  };
+}
+
+export default BoardApi;
