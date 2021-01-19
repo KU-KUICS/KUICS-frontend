8505b0d75001bb1f77926139d6ab623848d4051e
hyperflow <hyperflow@kakao.com>
2021-01-19 20:00:57 +0900

    feat(PostList): 글 작성 버튼 추가

---
+++ src/components/board/AddPostButton.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const AddPostButton = styled.button`
+  align-self: flex-end;
+  margin-top: 24px;
+
+  width: 100px;
+  height: 40px;
+
+  line-height: 40px;
+  text-align: center;
+  vertical-align: middle;
+
+  font-size: 16px;
+  font-weight: bold;
+
+  border-radius: 25px;
+  background: #b2f2bb;
+  color: #2b8a3e;
+`;
+
+export default AddPostButton;
