0c0587a7e71329a8bb8d20304e3cc5b381c2543a
hyperflow <hyperflow@kakao.com>
2021-01-23 02:17:20 +0900

    feat(WritePost): 게시글 작성 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import BoardPage from 'pages/BoardPage';
 import BoardPostPage from 'pages/BoardPostPage';
 import NoticePage from 'pages/NoticePage';
 import NotFoundPage from 'pages/NotFoundPage';
+import WritePostPage from 'pages/WritePostPage';
 
 const App: React.FC = () => {
   return (
@@ -15,7 +16,8 @@ const App: React.FC = () => {
       <GlobalStyle />
       <Switch>
         <Route component={MainPage} path="/" exact />
-        <Route component={BoardPostPage} path="/board/:postId" />
+        <Route component={WritePostPage} path="/:boardId/write" exact />
+        <Route component={BoardPostPage} path="/:boardId/:postId" exact />
         <Route component={BoardPage} path="/board" exact />
         <Route component={NoticePage} path="/notice" exact />
         <Route component={NotFoundPage} />
---
+++ src/components/post/WritePost.tsx
@@ -0,0 +1,40 @@
+import { Editor } from '@toast-ui/react-editor';
+import React from 'react';
+import styled from 'styled-components';
+import WritePostButton from 'components/board/WritePostButton';
+import { useHistory } from 'react-router-dom';
+
+const WritePostWrapper = styled.div`
+  padding: 24px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleInput = styled.input`
+  width: 100%;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 12px;
+  margin-bottom: 24px;
+  border-radius: 12px;
+  box-shadow: rgba(20, 20, 20, 0.05) 0px 6px 12px 0px;
+`;
+
+const WritePost: React.FC = () => {
+  return (
+    <WritePostWrapper>
+      <TitleInput placeholder="제목을 입력하세요" />
+      <Editor
+        height="500px"
+        placeholder="Markdown 문법으로 내용을 적어주세요!"
+        previewStyle="vertical"
+      />
+      <WritePostButton onClick={() => useHistory().push('/')}>
+        작성 완료!
+      </WritePostButton>
+    </WritePostWrapper>
+  );
+};
+
+export default WritePost;
